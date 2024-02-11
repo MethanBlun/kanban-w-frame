302132e0083cb72dc89a93e85ef4711e13a4abc1
Osirus Jack <methanblun@gmail.com>
2024-02-11 15:57:36 +0000

    initial commit

--- kanban-w-frame/src/App.jsx
+++ kanban-w-frame/src/App.jsx
@@ -6,32 +6,36 @@ import './App.css';
 
 
 const quotesList = [
+  "I'm convinced that about half of what separates the successful entrepreneurs from the nonsuccessful ones is pure perseverance.",
   "Your time is limited, so don’t waste it living someone else’s life.",
   "The only way to do great work is to love what you do.",
   "Innovation distinguishes between a leader and a follower.",
   "The best way to predict the future is to invent it.",
   "Stay hungry, stay foolish.",
+  "You can't just ask customers what they want and then try to give that to them. By the time you get it built, they'll want something new.",
+  "People think focus means saying yes to the thing you've got to focus on. It means saying no to the hundred other good ideas that there are. You have to pick carefully.",
+  "“We’re here to put a dent in the universe. Otherwise why else even be here?”"
 ];
 
 
 function App() {
 
 
-    // Choix initial de la citation aléatoire lors du chargement du composant
+   
     const initialQuote = getRandomQuote(quotesList);
     const [quote, setQuote] = useState(initialQuote);
   
-    // Fonction pour choisir aléatoirement une citation dans la liste
+    
     function getRandomQuote(quotes) {
       const randomIndex = Math.floor(Math.random() * quotes.length);
       return quotes[randomIndex];
     }
   
-    // Fonction pour mettre à jour la citation avec une nouvelle citation aléatoire
-    function updateQuote() {
-      const newQuote = getRandomQuote(quotesList);
-      setQuote(newQuote);
-    }
+   
+    // function updateQuote() {
+    //   const newQuote = getRandomQuote(quotesList);
+    //   setQuote(newQuote);
+    // }
 
 
   
@@ -49,6 +53,7 @@ function App() {
       </header>
       <main className="main">
         <div className="todo-list">
+          {/* TODO:jsjjjss */}
 
           <div className='adding-Task'>Task adder</div>
           <div className='doing-Task'>doing task</div>
@@ -59,7 +64,7 @@ function App() {
       </main>
       <footer className="footer">
         <div className="legal-section">
-          {/* Section pour la mention légale et le droit d'auteur */}
+        
           <p>Mention légale et droit d'auteur</p>
         </div>
       </footer>
